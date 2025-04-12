ee9d6f55899cdf9cbadce9ea2963f959eba5056e
Dev Poonia <110079684+uditbro@users.noreply.github.com>
2025-04-12 19:42:24 +0530

    Update settings.py

--- tourgenai/settings.py
+++ tourgenai/settings.py
@@ -30,10 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY","qwertyuiopasdfghjklzxcvbnm")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG",bool(True))
 
-ALLOWED_HOSTS = ["https://tourgeniephase1-backend.onrender.com"]
-CORS_ALLOWED_ORIGINS = [
-    "https://tourgeniephase1-frontend-jkoq.vercel.app/",
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS")
 
 
 load_dotenv()
